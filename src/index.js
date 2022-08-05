ccaec789f75c2db949dff3279ce06c17c568491c
Caleb Denio <cjdenio44@gmail.com>
2022-08-05 11:38:47 -0700

    color!

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+let color = "black";
+
 io.on("connection", (socket) => {
+  socket.emit("color", color);
+
   socket.on("emoji", (emoji) => {
     socket.broadcast.emit("emoji", emoji);
+  });
+
+  socket.on("color", (selectedColor) => {
+    color = selectedColor;
+    socket.broadcast.emit("color", selectedColor);
   });
 });
 
